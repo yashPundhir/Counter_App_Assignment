d32a78657bc4a21cf21d9097bb9e2c2b5ab1e766
yashPundhir <yash.pundhir.prof@gmail.com>
2023-07-10 19:19:35 +0530

    minor changes done in UI

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -9,24 +9,30 @@ const Counter = () => {
 		setCount(count - 1);
 	};
 	return (
-		<div>
-			<h1 className="mb-20">Basic Counter</h1>
-			<div className="flex justify-center gap-20">
-				<button
-					className="text-3xl border-2 border-red-500 flex justify-center items-center"
-					onClick={incrementCount}
-				>
-					+
-				</button>
-				<div className="text-3xl border-2 border-green-500 w-20 flex justify-center items-center rounded-lg">
-					{count}
+		<div className="flex justify-center items-center">
+			<div className="w-[1200px] h-[580px] bg-pink-100 rounded-[25px] flex justify-center items-center">
+				<div className="flex flex-col justify-center items-center bg-red-500 gap-10 w-[600px] h-[480px] rounded-[15px]">
+					<h1 className="text-white text-[55px] font-semibold">
+						Basic Counter
+					</h1>
+					<div className="text-[75px] flex justify-center items-center text-white">
+						{count}
+					</div>
+					<div className="flex gap-10">
+						<button
+							className="text-3xl border-2 border-red-500 flex justify-center items-center"
+							onClick={incrementCount}
+						>
+							Increment
+						</button>
+						<button
+							className="text-3xl border-2 border-red-500 flex justify-center items-center"
+							onClick={decrementCount}
+						>
+							Decrement
+						</button>
+					</div>
 				</div>
-				<button
-					className="text-3xl border-2 border-red-500 flex justify-center items-center"
-					onClick={decrementCount}
-				>
-					-
-				</button>
 			</div>
 		</div>
 	);
